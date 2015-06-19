1470ff6a2530efc22db300838dc1869a4570cf76
David Shin <dk.shin1992@gmail.com>
2015-06-19 18:32:52 +1200

    Create simulate.cpp

---
+++ simulate.cpp
@@ -0,0 +1,37 @@
+// simulate.cpp
+// CUPE simulation 
+
+#include "cupe.h"
+#include "toe.h"
+#include "processor.h"
+//include XMLParser
+
+// Command-line usage of CUPE
+void usage(void)
+{
+	// Print usage to user
+}
+
+int main(void)
+{
+	// Check if the arguments CUPE is called with are valid
+	// Otherwise tell user the usage of the program
+	usage();
+
+	// Parse input arguments
+		// Open input XML file as simulation initialiser
+		// Graphics on/off
+	
+	// Parse XML
+		// Initialise a world to simulate
+		// Initialise the physical laws
+		// Get simulation range--e.g. time range
+	// Close XML file
+
+	// Simulate physics of the world for the range of time given
+		// Call numerical solver to do calculation and evolve world
+		// Output world snapshot
+		// Graphical feedback?
+	
+	// Free memory and close files
+}
