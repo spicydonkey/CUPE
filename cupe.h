63a3a54a6577c5057410b4496fcd053e3d287a70
David Shin <dk.shin1992@gmail.com>
2015-06-21 12:14:25 +1200

    Update world structure

--- cupe.h
+++ cupe.h
@@ -1,4 +1,59 @@
 #ifndef CUPE_H
 #define CUPE_H
+#include <vector>
+#include <string>
+
+class Particle
+{
+private:
+	// Identifier
+	std::string mName;
+
+	// General physical properties
+	double mMass;
+	double mPosition[3];
+	double mVelocity[3];
+	double mAcceleration[3];
+	double mOrientation[3];
+
+	// Collision properties
+	double mRadius;
+	double mElasticity;
+
+	// Interaction properties
+	double mCharge;		// electrical charge
+};
+
+class Boundary
+{
+	enum BCtype { ground, wall, box };
+
+private:
+	// Boundary type
+	BCtype mType;
+	
+	// Boundary condition parameters
+	std::vector<double> mParams;
+};
+
+typedef std::vector<Particle> Ensemble;
+typedef std::vector<Boundary> Boundaries;
+
+class World 
+{
+
+private:
+	// Absolute clock
+	double mTime;
+
+	// Particles
+	Ensemble mEnsemble;
+
+	// Boundary conditions
+	Boundaries mBoundaryConditions;
+
+public:
+
+};
 
 #endif
